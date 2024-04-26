3fa36125c7f50054a75236985ae8b49a780caa7a
raj <144378753+rally-ui@users.noreply.github.com>
2024-04-26 04:23:24 +0000

    day!

---
+++ 100DaysOfCode/Day1/script.js
@@ -0,0 +1,14 @@
+const accountId=14466
+let accountEmail = "raj@gmail.com"
+var accountPassword= "1234"
+accountCity = "jaipur"
+
+accountEmail = "ra@gmail.com"
+
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+
+console.table([accountId,accountEmail.accountPassword,accountCity])
