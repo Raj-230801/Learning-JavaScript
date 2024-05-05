92e8665d7d9805497f6d9db96650de808c366ed5
rally.ui <rally.20012004@gmail.com>
2024-05-05 15:10:21 +0530

    Harry'sCode

--- 100DaysOfCode/day8/faultyCalculator.js
+++ 100DaysOfCode/day8/faultyCalculator.js
@@ -1,3 +1,4 @@
+//tip: for changing all the same word without selecting them individualy We can use command "ctrl+shift+l"
 let a = Number(prompt("enter first number"));
 let b = prompt("enter operator");
 let c = Number(prompt("Enter second number"));
@@ -6,37 +7,84 @@ let n = Math.random();
 if (n > 0.1) {
   if (b == "+") {
     r = a + c;
-    console.log(r);
+    alert(`result is ${r}`);
   }
   if (b == "-") {
     r = a - c;
-    console.log(r);
+    alert(`result is ${r}`);
   }
   if (b == "*") {
     r = a * c;
-    console.log(r);
+    alert(`result is ${r}`);
   }
   if (b == "/") {
     r = a / c;
-    console.log(r);
+    alert(`result is ${r}`);
   }
 } 
 
 else {
   if (b == "+") {
     r = a - c;
-    console.log(r);
+    alert(`result is ${r}`);
   }
   if (b == "-") {
     r = a / c;
-    console.log(r);
+    alert(`result is ${r}`);
   }
   if (b == "*") {
     r = a + c;
-    console.log(r);
+    alert(`result is ${r}`);
   }
   if (b == "/") {
     r = a * c;
-    console.log(r);
+    alert(`result is ${r}`);
   }
 }
+
+
+ //++++++++++++++++ Other way+++++++++++++++
+
+// let a = Number(prompt("enter first number"));
+// let b = prompt("enter operator");
+// let c = Number(prompt("Enter second number"));
+
+// let n = Math.random();
+
+// let obj = {
+//     "+":"-",
+//     "*":"+",
+//     "-":"/",
+//     "/":"*",
+// }
+// if(n>0.1){
+//     alert(`The result is ${eval(`${a} ${b} ${c}`)}\nand value of random is${n}`)
+// }
+// else{
+//     alert(`The result is ${eval(`${a} ${b} ${c}\nand value of random is${n}`)}`)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
