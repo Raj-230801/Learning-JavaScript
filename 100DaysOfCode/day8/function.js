e1fe5c9b91b49fe3f5833db54cf81c7ab6a44491
rally.ui <rally.20012004@gmail.com>
2024-05-05 13:52:10 +0530

    functionAndArrowFunc

---
+++ 100DaysOfCode/day8/function.js
@@ -0,0 +1,19 @@
+function nice(name){
+
+console.log(`hello guys\nmy name is ${name}`)
+}
+nice("raj")
+
+function sum(a,b){
+    console.log(a+b)
+    return a+b
+}
+result = sum(5,6) +5
+console.log(result)
+
+const func1 = (x) =>{
+    console.log(`i am an arrow function ${x}`)
+}
+func1(75)
+
+
