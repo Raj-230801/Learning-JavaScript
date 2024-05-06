ca9da2b34b90a9812ceb440db339fa22daa2b984
rally.ui <rally.20012004@gmail.com>
2024-05-06 23:23:56 +0530

    DOM

---
+++ 100DaysOfCode/day10/DOM.js
@@ -0,0 +1,16 @@
+// DOM - children,parent & sibling nodes 
+console.log(`hello`)
+document.body.childNodes[0]
+document.body.childNodes[1]
+document.body.childNodes[1].childNodes
+let cont = document.body.childNodes[1]
+cont
+cont
+cont.firstChild
+cont.lastChild
+cont.firstElementChild
+cont.firstElementChild.style.color = "red"
+cont.firstElementChild.style.backgroundColor = "green"
+document.body.firstElementChild.firstElementChild.nextElementSibling
+document.body.firstElementChild.
+document.body.firstElementChild.children[3].parentElement
