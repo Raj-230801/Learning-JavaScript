b84c49469f84923a26ee515bd3b8c74c58ca19cf
raj <144378753+Raj-230801@users.noreply.github.com>
2024-05-08 04:12:27 +0000

    day11

---
+++ 100DaysOfCode/day11/script.js
@@ -0,0 +1,7 @@
+console.log("raj")
+
+// let boxes=document.getElementsByClassName("box")
+// console.log(boxes)
+
+// boxes[2].computedStyleMap.backgoundColor="red"
+document.getElementById("red").style.backgroundColor = "red"
